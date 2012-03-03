1835f768959589ae8c8fc3b36421225232051931
Nicolas Pinto <pinto@alum.mit.edu>
2012-03-03 02:57:06 -0500

    na

--- sthor/operation/fbcorr.py
+++ sthor/operation/fbcorr.py
@@ -41,34 +41,34 @@ except NameError:
     pass
 
 
-def main():
-    #arr_in = np.random.randn(200, 200, 64).astype('f')
-    arr_in = np.random.randn(20, 20, 64).astype('f')
-    fb = np.random.randn(9, 9, 64, 128).astype('f')
+#def main():
+    ##arr_in = np.random.randn(200, 200, 64).astype('f')
+    #arr_in = np.random.randn(20, 20, 64).astype('f')
+    #fb = np.random.randn(9, 9, 64, 128).astype('f')
 
-    fb2 = np.ascontiguousarray(fb.transpose(3, 0, 1, 2).copy())
-    print fb2.shape
-    print fb.shape
+    #fb2 = np.ascontiguousarray(fb.transpose(3, 0, 1, 2).copy())
+    #print fb2.shape
+    #print fb.shape
  
-    from pythor3.operation import fbcorr as fbcorr_pt3
+    #from pythor3.operation import fbcorr as fbcorr_pt3
 
-    import time
-    N = 10
-    start = time.time()
-    for i in xrange(N):
-        print i
-        #gv = fbcorr(arr_in, fb)
-        gt = fbcorr_pt3(arr_in, fb2, plugin='cthor', plugin_kwargs=dict(variant='sse:tbb'))[:]
-        #print np.linalg.norm(gv - gt)
-    end = time.time()
-    fps = N / (end - start)
-    print fps
-    tim = 1. / fps
-    print tim
+    #import time
+    #N = 10
+    #start = time.time()
+    #for i in xrange(N):
+        #print i
+        ##gv = fbcorr(arr_in, fb)
+        #gt = fbcorr_pt3(arr_in, fb2, plugin='cthor', plugin_kwargs=dict(variant='sse:tbb'))[:]
+        ##print np.linalg.norm(gv - gt)
+    #end = time.time()
+    #fps = N / (end - start)
+    #print fps
+    #tim = 1. / fps
+    #print tim
 
-    flops = np.cumprod(arr_in.shape[:2] + fb.shape)[-1] * 2
-    gflops = (flops / 1e9)
-    print 'gflops / sec', 1. * gflops / tim
+    #flops = np.cumprod(arr_in.shape[:2] + fb.shape)[-1] * 2
+    #gflops = (flops / 1e9)
+    #print 'gflops / sec', 1. * gflops / tim
 
-if __name__ == '__main__':
-    main()
+#if __name__ == '__main__':
+    #main()
