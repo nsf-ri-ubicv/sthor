abe3530cdeaf24f84e7cba7f252e7c345413c942
Nicolas Pinto <pinto@alum.mit.edu>
2012-03-05 03:09:25 -0500

    na

--- sthor/operation/fbcorr.py
+++ sthor/operation/fbcorr.py
@@ -1,10 +1,19 @@
+"""Filterbank Correlation Operation"""
+
+# Authors: Nicolas Pinto <nicolas.pinto@gmail.com>
+#          Nicolas Poilvert <nicolas.poilvert@gmail.com>
+#
+# License: BSD
+
+__all__ = ['fbcorr3']
+
 import numpy as np
 from skimage.util.shape import view_as_windows
 
 DEFAULT_STRIDE = 1
 
 
-def fbcorr(arr_in, arr_fb, stride=DEFAULT_STRIDE, arr_out=None):
+def fbcorr3(arr_in, arr_fb, stride=DEFAULT_STRIDE, arr_out=None):
     """3D Filterbank Correlation
     XXX: docstring
     """
@@ -42,39 +51,6 @@ def fbcorr(arr_in, arr_fb, stride=DEFAULT_STRIDE, arr_out=None):
 
 
 try:
-    fbcorr = profile(fbcorr)
+    fbcorr3 = profile(fbcorr3)
 except NameError:
     pass
-
-
-#def main():
-    ##arr_in = np.random.randn(200, 200, 64).astype('f')
-    #arr_in = np.random.randn(20, 20, 64).astype('f')
-    #fb = np.random.randn(9, 9, 64, 128).astype('f')
-
-    #fb2 = np.ascontiguousarray(fb.transpose(3, 0, 1, 2).copy())
-    #print fb2.shape
-    #print fb.shape
- 
-    #from pythor3.operation import fbcorr as fbcorr_pt3
-
-    #import time
-    #N = 10
-    #start = time.time()
-    #for i in xrange(N):
-        #print i
-        ##gv = fbcorr(arr_in, fb)
-        #gt = fbcorr_pt3(arr_in, fb2, plugin='cthor', plugin_kwargs=dict(variant='sse:tbb'))[:]
-        ##print np.linalg.norm(gv - gt)
-    #end = time.time()
-    #fps = N / (end - start)
-    #print fps
-    #tim = 1. / fps
-    #print tim
-
-    #flops = np.cumprod(arr_in.shape[:2] + fb.shape)[-1] * 2
-    #gflops = (flops / 1e9)
-    #print 'gflops / sec', 1. * gflops / tim
-
-#if __name__ == '__main__':
-    #main()
