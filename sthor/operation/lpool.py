48a87f07a74e83a29db303a3be4b31db58747f06
Nicolas Pinto <pinto@alum.mit.edu>
2012-03-05 05:29:19 -0500

    na

--- sthor/operation/lpool.py
+++ sthor/operation/lpool.py
@@ -8,19 +8,17 @@
 __all__ = ['lpool3']
 
 import numpy as np
-import numexpr as ne
 from skimage.util.shape import view_as_windows
 
+import numexpr as ne
+if not ne.use_vml:
+    import warnings
+    warnings.warn("numexpr is NOT using Intel VML!")
+
+# --
 DEFAULT_STRIDE = 1
 DEFAULT_ORDER = 1.0
 
-assert ne.use_vml
-#ne.set_num_threads(8)
-#set_vml_accuracy_mode(mode): Set the accuracy for VML operations.
-#The mode parameter can take the values:
-#- 'low': Equivalent to VML_LA - low accuracy VML functions are called
-#- 'high': Equivalent to VML_HA - high accuracy VML functions are called
-#- 'fast': Equivalent to VML_EP - enhanced performance VML functions are called
 
 def lpool3(arr_in, neighborhood,
            order=DEFAULT_ORDER,
@@ -45,13 +43,10 @@ def lpool3(arr_in, neighborhood,
         # XXX: more tests on the shape of arr_out
 
     _arr_out = ne.evaluate('arr_in ** order')
-    #_arr_out = np.squeeze(view_as_windows(_arr_out, (1, nbw, 1)).sum(-2))[:, ::stride]
-    rv = view_as_windows(_arr_out, (1, nbw, 1))
-    _arr_out = ne.evaluate('sum(rv, 4)')[:, ::stride, :, 0, 0]
-    #_arr_out = np.squeeze(view_as_windows(_arr_out, (nbh, 1, 1)).sum(-3))[::stride, :]
-    rv = view_as_windows(_arr_out, (nbh, 1, 1))
-    #_arr_out = np.squeeze(ne.evaluate('sum(rv, 3)'))[::stride]
-    _arr_out = ne.evaluate('sum(rv, 3)')[::stride, :, :, 0, 0]
+    _arr_out = view_as_windows(_arr_out, (1, nbw, 1))
+    _arr_out = ne.evaluate('sum(_arr_out, 4)')[:, ::stride, :, 0, 0]
+    _arr_out = view_as_windows(_arr_out, (nbh, 1, 1))
+    _arr_out = ne.evaluate('sum(_arr_out, 3)')[::stride, :, :, 0, 0]
     # Note that you need to use '1' and not '1.0' so that the dtype of
     # the exponent does not change (i.e. get promoted)
     _arr_out = ne.evaluate('_arr_out ** (1 / order)')
@@ -62,6 +57,7 @@ def lpool3(arr_in, neighborhood,
         arr_out = _arr_out
 
     assert arr_out.dtype == arr_in.dtype
+
     return arr_out
 
 try:
