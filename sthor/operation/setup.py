182de6cc1483b190dc8983e424fc334f9c5f4949
Nicolas Pinto <pinto@alum.mit.edu>
2012-03-05 05:55:38 -0500

    na

--- sthor/operation/setup.py
+++ sthor/operation/setup.py
@@ -6,16 +6,12 @@ base_path = os.path.abspath(os.path.dirname(__file__))
 
 def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import Configuration, get_numpy_include_dirs
-    #from sthor._build import cython
-    #import build
-    import skimage._build as build
-    #import sthor.util._build as build
+    from skimage._build import cython
 
     config = Configuration('resample', parent_package, top_path)
     config.add_data_dir('tests')
 
-    build.cython(['_resample.pyx'], working_path=base_path)
-    #cython(['_resample.pyx'], working_path=base_path)
+    cython(['_resample.pyx'], working_path=base_path)
 
     config.add_extension(
         '_resample',
