3bf5df46070a08dc8d34e4eb5a4c02e97dfbac56
Nicolas Pinto <pinto@alum.mit.edu>
2012-06-25 14:03:28 -0400

    no comment

---
+++ sthor/tensordot_bench.py
@@ -0,0 +1,15 @@
+import time
+import numpy as np
+from skimage.util.shape import view_as_windows
+
+a = np.random.randn(512, 512).astype('f')
+fb = np.random.randn(512, 32, 32).astype('f')
+
+start = time.time()
+out = np.tensordot(view_as_windows(a, fb.shape[1:]), fb, axes=[(2, 3), (1, 2)])
+end = time.time()
+
+duration = end - start
+gflops = (np.prod(out.shape) * np.prod(fb.shape[1:]) * 2) / (1000**3.)
+gflops_per_sec = gflops / duration
+print 'gflops/sec', gflops_per_sec
